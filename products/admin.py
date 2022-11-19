06e29f0b5829b6b2b82a29068b0195de44508d34
SCollins <sje.collins@gmail.com>
2022-11-19 20:24:17 +0000

    Register products models with admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, SubCategory, ProductVariant, Product
+
+
+@admin.register(ProductVariant)
+class ProductVariantAdmin(admin.ModelAdmin):
+    list_filter = ('size',)
+    list_display = ('size', 'price',)
+    search_fields = ('size',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_filter = ('category', 'subcategory', 'in_stock',)
+    list_display = ('name', 'category', 'subcategory', 'current_stock',
+                    'in_stock')
+    search_fields = ('name', 'category', 'subcategory',)
+
+
+admin.site.register(Category)
+admin.site.register(SubCategory)

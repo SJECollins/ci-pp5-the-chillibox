cdd2f94d498a5dec0055d18daf19bd1f8f83d1a4
SCollins <sje.collins@gmail.com>
2022-11-19 19:58:01 +0000

    Edit category & latest product html, add link styles, link views in header

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 app_name = 'products'
 urlpatterns = [
-    path('', views.LatestProducts.as_view(), name='all'),
+    path('', views.LatestProducts.as_view(), name='latest'),
     path('category/<str:slug>', views.CategoryView.as_view(), name='category'),
 ]

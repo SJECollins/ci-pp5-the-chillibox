3511cfe0076dd6f77015ba8f1f632e17d78b8477
SCollins <sje.collins@gmail.com>
2022-11-20 13:36:07 +0000

    Edit IndexView to include latest products

--- home/views.py
+++ home/views.py
@@ -1,9 +1,15 @@
+
 from django.shortcuts import render
 from django.views import View
+
+from products.models import Product
 
 
 class IndexView(View):
     def get(self, request):
+        latest_products = Product.objects.all().order_by('-added_on')[0:4]
         template_name = 'home/index.html'
-        context = {}
+        context = {
+            'latest_products': latest_products,
+        }
         return render(request, template_name, context)

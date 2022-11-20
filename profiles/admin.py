7d1e5c2d5d1724bbc0905572dff22454b3bac43b
SCollins <sje.collins@gmail.com>
2022-11-20 13:50:05 +0000

    Register UserProfile with admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserProfile
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_filter = ('country', )
+    list_display = ('first_name', 'last_name', 'town_or_city', 'country',
+                    'postcode',)
+    search_fields = ('last_name', 'town_or_city', 'country', 'postcode')

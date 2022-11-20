7eb217369ed5f7abc3816ce58ce5e7ee15f8d4d2
SCollins <sje.collins@gmail.com>
2022-11-20 12:15:32 +0000

    Update header links. Add profiles urls

--- thechillibox/urls.py
+++ thechillibox/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('home.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
+    path('profiles/', include('profiles.urls')),
     path('products/', include('products.urls')),
+
 ]

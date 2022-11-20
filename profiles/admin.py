e353b28d27868d64473f50d46adc416d3b5a47ed
SCollins <sje.collins@gmail.com>
2022-11-20 13:51:05 +0000

    Edit profiles.admin to add user to list_display

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 from .models import UserProfile
 
+
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_filter = ('country', )
-    list_display = ('first_name', 'last_name', 'town_or_city', 'country',
-                    'postcode',)
+    list_display = ('user', 'first_name', 'last_name', 'town_or_city',
+                    'country', 'postcode',)
     search_fields = ('last_name', 'town_or_city', 'country', 'postcode')

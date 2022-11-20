41d4dadbb48d3e9ac399666e24d1f34ade38d86d
SCollins <sje.collins@gmail.com>
2022-11-20 11:47:41 +0000

    Add ProfileView and profile template

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
 
-# Create your views here.
+from .models import UserProfile
+
+
+class ProfileView(LoginRequiredMixin, View):
+    def get(self, request):
+        profile = get_object_or_404(UserProfile, user=request.user)
+        template_name = 'profiles/profile.html'
+        context = {'profile': profile, }
+        return render(request, template_name, context)

ba6ff083732edefa6f288610a1c1370ec8a48aca
SCollins <sje.collins@gmail.com>
2022-11-28 23:54:47 +0000

    Update check_stock method

--- cart/models.py
+++ cart/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -12,7 +13,8 @@ class HeldCart(models.Model):
     hold_time_start = models.DateTimeField(auto_now_add=True)
 
     def check_time(self):
-        return (timezone.now() - self.hold_time_start) > settings.CART_HOLD_TIME_MINUTES
+        time_passed = (timezone.now() - self.hold_time_start)
+        return (time_passed.total_seconds() / 60.0) > settings.CART_HOLD_TIME_MINUTES
 
 
 class HeldItems(models.Model):

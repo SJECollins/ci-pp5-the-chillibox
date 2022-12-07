e631496e8b6ac6d54da94f3f94922c19ab9b49af
SCollins <sje.collins@gmail.com>
2022-12-07 21:27:22 +0000

    Add test for add_to_cart in cart.test_views
    - Fix cart_canvas template in case of missing thumbnail per error found during test

--- cart/test_views.py
+++ cart/test_views.py
@@ -1,4 +1,9 @@
+import datetime
+import pytz
 from django.test import TestCase
+from django.urls import reverse
+
+from products.models import Category, Product, Variant
 
 
 class TestCart(TestCase):
@@ -11,3 +16,47 @@ class TestCart(TestCase):
         self.assertTemplateUsed(response, 'cart/cart.html')
         self.assertTemplateUsed(response, 'includes/header.html')
         self.assertTemplateUsed(response, 'includes/footer.html')
+
+
+class TestCartFunctions(TestCase):
+    def setUp(self):
+        self.category_a = Category.objects.create(
+            name='Test Category',
+            slug='test-cat'
+        )
+        self.category_a.save()
+        self.product_a = Product.objects.create(
+            category=self.category_a,
+            name='Test Pepper',
+            slug='test-pepper',
+            added_on=datetime.datetime(2021, 12, 3, 0, 0, 0, tzinfo=pytz.utc),
+        )
+        self.product_a.save()
+        self.variant_a = Variant.objects.create(
+            product=self.product_a,
+            size='small',
+            price=2.99,
+            current_stock=10,
+            in_stock=True,
+        )
+        self.variant_a.save()
+
+    def test_add_to_cart(self):
+        item_id = self.product_a.id
+        slug = self.product_a.slug
+        data = {
+            'product_variant': self.variant_a.id,
+            'quantity': 1,
+        }
+        cart_before = self.client.session.get('cart', {})
+        self.assertEqual(len(cart_before), 0)
+        response = self.client.post(reverse('cart:add_to_cart',
+                                    args=[item_id]), data)
+        self.assertRedirects(
+            response, '/products/product/test-pepper',
+            status_code=302, target_status_code=200,
+            fetch_redirect_response=True,)
+        cart_after = self.client.session.get('cart', {})
+        self.assertEqual(len(cart_after), 1)
+        print(cart_after)
+

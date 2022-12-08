13c82d95aac4319adeb8e38fa04f976b6d91f85d
SCollins <sje.collins@gmail.com>
2022-12-08 13:29:51 +0000

    Add tests for checkout.test_views
    - Add additional tests for checkout views for when items in cart and user is logged in

--- checkout/test_views.py
+++ checkout/test_views.py
@@ -2,7 +2,9 @@ import datetime
 import pytz
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
 
+from profiles.models import UserProfile
 from products.models import Category, Product, Variant
 from cart.models import HeldCart, HeldItems
 from .models import Order, OrderLineItem
@@ -64,9 +66,17 @@ class TestCheckoutCartFull(TestCase):
         self.variant_a.save()
         self.held_cart = HeldCart.objects.create(
             cart_key=self.client.session.session_key,
-            hold_time_start=datetime.datetime(2022, 12, 3, 0, 0, 0, tzinfo=pytz.utc),
+            hold_time_start=datetime.datetime(2022, 12, 3, 0, 0, 0,
+                                              tzinfo=pytz.utc),
         )
         self.held_cart.save()
+        self.held_item = HeldItems.objects.create(
+            cart=self.held_cart,
+            product=self.product_a,
+            variant=self.variant_a,
+            qty=1,
+        )
+        self.held_item.save()
         self.order = Order.objects.create(
             order_number='xyz',
             first_name='Test',
@@ -90,11 +100,153 @@ class TestCheckoutCartFull(TestCase):
         )
         self.order_line_item.save()
 
+    def test_checkout(self):
+        """
+        Try to view checkout with items in cart.
+        """
+        session = self.client.session
+        session['cart'] = {
+            '1': {'items_by_size': {'small': 1}}
+        }
+        session.save()
+        response = self.client.get('/checkout/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'checkout/checkout.html')
+        self.assertTemplateUsed(response, 'includes/header.html')
+        self.assertTemplateUsed(response, 'includes/footer.html')
+
+    def test_checkout_post(self):
+        session = self.client.session
+        session['cart'] = {
+            '1': {'items_by_size': {'small': 1}}
+        }
+        session.save()
+        data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'email': 'test@email.com',
+            'phone_number': '1234567',
+            'street_address1': '1 First St',
+            'street_address2': '',
+            'town_or_city': 'SomeTown',
+            'postcode': '',
+            'country': 'IE',
+            'county': '',
+        }
+        self.assertEqual(Order.objects.count(), 1)
+        response = self.client.post(reverse('checkout:checkout'), data)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Order.objects.count(), 2)
+
     def test_checkout_success_view(self):
         """
-        Try to get checkout success view with.
-        No order number so should render a 404.
+        Try to get checkout success view with order number
         """
+        order_no = self.order.order_number
+        response = self.client.get(
+            reverse('checkout:checkout_success', args=[order_no]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'checkout/checkout_success.html')
+        self.assertTemplateUsed(response, 'includes/header.html')
+        self.assertTemplateUsed(response, 'includes/footer.html')
+
+
+class TestCheckoutLoggedIn(TestCase):
+    """
+    Test checkout for a logged in user.
+    """
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='test@email.com',
+            password='testpass1'
+        )
+        self.user.is_staff = True
+        self.user.save()
+        self.client.login(email='test@email.com', password='testpass1')
+        self.category_a = Category.objects.create(
+            name='Test Category',
+            slug='test-cat'
+        )
+        self.category_a.save()
+        self.product_a = Product.objects.create(
+            category=self.category_a,
+            name='Test Pepper',
+            slug='test-pepper',
+            added_on=datetime.datetime(2021, 12, 3, 0, 0, 0, tzinfo=pytz.utc),
+        )
+        self.product_a.save()
+        self.variant_a = Variant.objects.create(
+            product=self.product_a,
+            size='small',
+            price=2.99,
+            current_stock=10,
+            in_stock=True,
+        )
+        self.variant_a.save()
+        self.held_cart = HeldCart.objects.create(
+            cart_key=self.client.session.session_key,
+            hold_time_start=datetime.datetime(2022, 12, 3, 0, 0, 0,
+                                              tzinfo=pytz.utc),
+        )
+        self.held_cart.save()
+        self.held_item = HeldItems.objects.create(
+            cart=self.held_cart,
+            product=self.product_a,
+            variant=self.variant_a,
+            qty=1,
+        )
+        self.held_item.save()
+        self.order = Order.objects.create(
+            order_number='xyz',
+            first_name='Test',
+            last_name='User',
+            email='test@email.com',
+            phone_number='1111111',
+            country='IE',
+            town_or_city='SomeTown',
+            street_address1='1 First St',
+            date=datetime.datetime(2022, 12, 3, 0, 0, 0, tzinfo=pytz.utc),
+            order_total=2.99,
+            grand_total=7.49,
+        )
+        self.order.save()
+        self.order_line_item = OrderLineItem.objects.create(
+            order=self.order,
+            product=self.product_a,
+            variant=self.variant_a,
+            quantity=1,
+            lineitem_total=2.99,
+        )
+        self.order_line_item.save()
+
+    def test_checkout_logged_in(self):
+        profile = UserProfile.objects.get(user=self.user)
+        profile.first_name = 'Test'
+        profile.last_name = 'User'
+        profile.phone_number = '1111111'
+        profile.country = 'IE'
+        profile.town_or_city = 'SomeTown'
+        profile.street_address1 = '1 First St'
+        profile.save()
+        session = self.client.session
+        session['cart'] = {
+            '1': {'items_by_size': {'small': 1}}
+        }
+        session.save()
+        response = self.client.get('/checkout/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'checkout/checkout.html')
+        self.assertTemplateUsed(response, 'includes/header.html')
+        self.assertTemplateUsed(response, 'includes/footer.html')
+
+    def test_checkout_success_logged_in(self):
+        """
+        Try to get checkout success view with order number
+        """
+        session = self.client.session
+        session['save_info'] = True
+        session.save()
         order_no = self.order.order_number
         response = self.client.get(
             reverse('checkout:checkout_success', args=[order_no]))

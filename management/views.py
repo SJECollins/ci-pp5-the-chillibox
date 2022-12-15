3dbd6ffa5301041aa1b91b91a0c0aa2927f7f46e
SCollins <sje.collins@gmail.com>
2022-12-15 20:32:40 +0000

    Add product dashboard order by category & subcategory

--- management/views.py
+++ management/views.py
@@ -18,7 +18,7 @@ class ProductDashboard(StaffRequiredMixin, View):
     View for admin accessible management dashboard.
     """
     def get(self, request):
-        products = Product.objects.all()
+        products = Product.objects.all().order_by('category', 'subcategory')
         template_name = 'management/dashboard.html'
         context = {
             'products': products,

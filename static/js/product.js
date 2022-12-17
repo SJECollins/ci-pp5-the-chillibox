2d205970092cb4e827ce9e960a2914776a108d27
SCollins <sje.collins@gmail.com>
2022-12-17 16:08:59 +0000

    Refactor product.js

--- static/js/product.js
+++ static/js/product.js
@@ -2,13 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const quantity = document.getElementById("product_qty")
     const decrease = document.getElementById("dec_qty")
     const increase = document.getElementById("inc_qty")
-    const prices = document.getElementById("prices")
     const addBtn = document.getElementById("add")
     const warning = document.getElementById("qty_warning")
     const stock = document.getElementById("current_stock")
 
     // Mutation Observer for stock - https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
-    // FIVE HOURS TO FIND A SOLUTION NOT INVOLVING jQuery
     const config = { childList: true }
     const callback = (mutationList, observer) => {
         for (const mutation of mutationList) {
@@ -22,31 +20,27 @@ document.addEventListener("DOMContentLoaded", () => {
     const observer = new MutationObserver(callback)
     observer.observe(stock, config)
 
-    prices.addEventListener("change", changeAddBtn)
-
-    function changeAddBtn() {
-        if (prices.value == "default") {
-            addBtn.disabled = true
-        } else {
-            addBtn.disabled = false
-        }
-    }
-
     decrease.addEventListener("click", decQty)
     increase.addEventListener("click", incQty)
 
     let qty = Number(quantity.value)
     let currentStock
+    warning.style.display = "none"
 
     function updateStock() {
         currentStock = stock.textContent
-    }
-
-    if (currentStock == 0) {
-        quantity.value = 0
-        warning.style.display = "block"
-    } else {
-        warning.style.display = "none"
+        if (currentStock == "") {
+            addBtn.disabled = true
+            warning.style.display = "none"
+        } else if (currentStock <= 0) {
+            quantity.textContent = 0
+            warning.style.display = "block"
+            addBtn.disabled = true
+        } else {
+            quantity.textContent = 1
+            warning.style.display = "none"
+            addBtn.disabled = false
+        }        
     }
 
     function decQty() {
